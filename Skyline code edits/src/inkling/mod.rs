31d4abf58d511372a121662f22dec95d39187e98
scootgewp <diegoazael732@gmail.com>
2020-08-13 23:49:59 -0500

    Inkling but finished i think

--- Skyline code edits/src/inkling/mod.rs
+++ Skyline code edits/src/inkling/mod.rs
@@ -2,6 +2,7 @@ use smash::hash40;
 use smash::lib::lua_const::*;
 use smash::lua2cpp::L2CFighterCommon;
 use acmd::{acmd, acmd_func};
+use smash::lua2cpp::L2CFighterBase;
 
 #[acmd::acmd_func(
     battle_object_category = BATTLE_OBJECT_CATEGORY_FIGHTER, 
@@ -11,7 +12,7 @@ use acmd::{acmd, acmd_func};
 pub fn inkling_fair(fighter: &mut L2CFighterCommon) {
     acmd!({
         if(is_excute){
-        frame(Frame=5) FT_MOTION_RATE(FSM=0.8)    
+        FT_MOTION_RATE(FSM=0.8)    
         }
         frame(Frame=5) 
         if(is_excute){
@@ -19,7 +20,7 @@ pub fn inkling_fair(fighter: &mut L2CFighterCommon) {
         }
         frame(Frame=10)
         if(is_excute){
-        frame(Frame=5) FT_MOTION_RATE(FSM=1)
+        FT_MOTION_RATE(FSM=1)
         ATTACK(ID=0, Part=0, Bone=hash40("top"), Damage=10.0, Angle=361, KBG=90, FKB=0, BKB=30, Size=3.6, X=0.0, Y=7.0, Z=4.0, X2=0.0, Y2=7.0, Z2=6.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_M, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_KICK)
         ATTACK(ID=1, Part=0, Bone=hash40("top"), Damage=12.0, Angle=361, KBG=90, FKB=0, BKB=30, Size=5.0, X=0.0, Y=6.5, Z=10.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_L, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_KICK)
         }
@@ -55,7 +56,7 @@ pub fn inkling_utilt(fighter: &mut L2CFighterCommon) {
         ATTACK(ID=1, Part=0, Bone=hash40("legl"), Damage=6.0, Angle=70, KBG=95, FKB=0, BKB=55, Size=4.0, X=2.0, Y=0.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_M, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_KICK)
         AttackModule::set_attack_height_all(ATTACK_HEIGHT_HIGH, false)
         }
-        wait(Frames=6)
+        wait(Frames=8)
         if(is_excute){
         AttackModule::clear_all()
         }        
@@ -131,12 +132,16 @@ pub fn inkling_uair(fighter: &mut L2CFighterCommon) {
     animcmd = "game_attackairlw")]
 pub fn inkling_dair(fighter: &mut L2CFighterCommon) {
     acmd!({
+        if(is_excute){
+        FT_MOTION_RATE(FSM=0.8)    
+        }
         frame(Frame=5)
         if(is_excute){
         WorkModule::on_flag(Flag=FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING)
         }
         frame(Frame=16)
         if(is_excute){
+        FT_MOTION_RATE(FSM=1) 
         ATTACK(ID=0, Part=0, Bone=hash40("top"), Damage=10.0, Angle=80, KBG=60, FKB=0, BKB=60, Size=4.0, X=0.0, Y=4.5, Z=0.5, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_L, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
         ATTACK(ID=1, Part=0, Bone=hash40("top"), Damage=12.0, Angle=270, KBG=100, FKB=0, BKB=30, Size=5.0, X=0.0, Y=-2.0, Z=1.0, X2=0.0, Y2=-4.0, Z2=1.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_L, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
         }
@@ -152,9 +157,141 @@ pub fn inkling_dair(fighter: &mut L2CFighterCommon) {
     });
 }
 
+#[acmd::acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_FIGHTER, 
+    battle_object_kind = FIGHTER_KIND_INKLING, 
+    animation = "special_hi_landing",
+    animcmd = "game_specialhilanding")]
+pub fn inkling_uspecial_landing(fighter: &mut L2CFighterCommon) {
+    acmd!({
+            frame(Frame=20)
+            if(is_excute){
+            ArticleModule::set_visibility_whole(FIGHTER_INKLING_GENERATE_ARTICLE_SQUID, false)
+            }   
+    });
+}
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_SPLASH, 
+    animation = "normal",
+    animcmd = "game_normal")]
+pub fn inkling_splash(fighter: &mut L2CFighterBase) {
+    acmd!({
+        if(is_excute){
+            ATTACK(ID=0, Part=0, Bone=hash40("top"), Damage=8.0, Angle=80, KBG=100, FKB=0, BKB=25, Size=6.0, X=0.0, Y=2.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=0.5, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_SPEED, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=true, Direct_Hitbox=false, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_PUNCH, Type=ATTACK_REGION_WATER)
+            AttackModule::set_ink_value(ID=0, Ink=50)
+            }
+            frame(Frame=3)
+            if(is_excute){
+            ATTACK(ID=0, Part=0, Bone=hash40("top"), Damage=6.0, Angle=80, KBG=100, FKB=0, BKB=25, Size=6.0, X=0.0, Y=2.0, Z=-7.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=0.5, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_SPEED, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=true, Direct_Hitbox=false, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_PUNCH, Type=ATTACK_REGION_WATER)
+            ATTACK(ID=1, Part=0, Bone=hash40("top"), Damage=6.0, Angle=80, KBG=100, FKB=0, BKB=25, Size=6.0, X=0.0, Y=2.0, Z=7.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=0.5, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_SPEED, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=true, Direct_Hitbox=false, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_PUNCH, Type=ATTACK_REGION_WATER)
+            AttackModule::set_ink_value(ID=0, Ink=40)
+            AttackModule::set_ink_value(ID=1, Ink=40)
+            }
+             
+    });
+}
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_ROLLER, 
+    animation = "special_s_walk",
+    animcmd = "game_specialswalk")]
+pub fn inkling_roller_gw(fighter: &mut L2CFighterBase) {
+    acmd!({
+        if(is_excute){
+            ATTACK(ID=0, Part=0, Bone=hash40("neck"), Damage=4.0, Angle=80, KBG=40, FKB=0, BKB=100, Size=3.6, X=0.5, Y=3.8, Z=0.0, X2=0.5, Y2=3.8, Z2=-2.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_F, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=40, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
+            AttackModule::set_ink_value(ID=0, Ink=60)
+            AttackModule::set_ink_value(ID=1, Ink=60)
+            } 
+    });
+}
+
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_ROLLER, 
+    animation = "special_s_dash",
+    animcmd = "game_specialsdash")]
+pub fn inkling_roller_gd(fighter: &mut L2CFighterBase) {
+    acmd!({
+        frame(Frame=4)
+        if(is_excute){
+        ATTACK(ID=0, Part=0, Bone=hash40("neck"), Damage=8.0, Angle=80, KBG=40, FKB=0, BKB=100, Size=3.6, X=0.5, Y=3.8, Z=5.0, X2=0.5, Y2=3.8, Z2=-5.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_F, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=40, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_M, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
+        AttackModule::set_ink_value(ID=0, Ink=120)
+        AttackModule::set_ink_value(ID=1, Ink=120)
+        } 
+    });
+}
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_ROLLER, 
+    animation = "special_s_run",
+    animcmd = "game_specialsrun")]
+pub fn inkling_roller_gr(fighter: &mut L2CFighterBase) {
+    acmd!({
+        if(is_excute){
+            ATTACK(ID=0, Part=0, Bone=hash40("neck"), Damage=11.0, Angle=80, KBG=40, FKB=0, BKB=100, Size=3.6, X=0.5, Y=3.8, Z=5.0, X2=0.5, Y2=3.8, Z2=-5.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_F, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=40, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_L, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
+            AttackModule::set_ink_value(ID=0, Ink=100)
+            AttackModule::set_ink_value(ID=1, Ink=100)
+            }
+             
+    });
+}
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_ROLLER, 
+    animation = "special_air_s_walk",
+    animcmd = "game_specialairswalk")]
+pub fn inkling_roller_aw(fighter: &mut L2CFighterBase) {
+    acmd!({
+        if(is_excute){
+            ATTACK(ID=0, Part=0, Bone=hash40("roll"), Damage=4.0, Angle=290, KBG=60, FKB=0, BKB=20, Size=3.6, X=0.0, Y=0.0, Z=0.0, X2=0.0, Y2=0.0, Z2=0.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_F, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=40, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
+            AttackModule::set_ink_value(ID=0, Ink=60)
+            AttackModule::set_ink_value(ID=1, Ink=60)
+            } 
+    });
+}
+
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_ROLLER, 
+    animation = "special_air_s_dash",
+    animcmd = "game_specialairsdash")]
+pub fn inkling_roller_ad(fighter: &mut L2CFighterBase) {
+    acmd!({
+        frame(Frame=4)
+        if(is_excute){
+        ATTACK(ID=0, Part=0, Bone=hash40("roll"), Damage=14.0, Angle=290, KBG=90, FKB=0, BKB=20, Size=3.6, X=0.0, Y=0.0, Z=5.0, X2=0.0, Y2=0.0, Z2=-5.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_F, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=40, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_M, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
+        AttackModule::set_ink_value(ID=0, Ink=120)
+        AttackModule::set_ink_value(ID=1, Ink=120)
+        }
+         
+    });
+}
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_WEAPON, 
+    battle_object_kind = WEAPON_KIND_INKLING_ROLLER, 
+    animation = "special_air_s_run",
+    animcmd = "game_specialairsrun")]
+pub fn inkling_roller_ar(fighter: &mut L2CFighterBase) {
+    acmd!({
+        if(is_excute){
+            ATTACK(ID=0, Part=0, Bone=hash40("roll"), Damage=11.0, Angle=290, KBG=33, FKB=0, BKB=90, Size=3.6, X=0.0, Y=0.0, Z=5.0, X2=0.0, Y2=0.0, Z2=-5.0, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_F, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=40, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_ink_hit"), SFXLevel=ATTACK_SOUND_LEVEL_L, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_OBJECT)
+            AttackModule::set_ink_value(ID=0, Ink=100)
+            AttackModule::set_ink_value(ID=1, Ink=100)
+            }
+             
+    });
+}
 
 pub fn install() {
     acmd::add_hooks!(
-inkling_bair, inkling_dair, inkling_fair, inkling_uair, inkling_utilt        
+inkling_bair, inkling_dair, inkling_fair, inkling_roller_ad, inkling_roller_ar, inkling_roller_aw, inkling_roller_gd, inkling_roller_gr, inkling_roller_gw, inkling_splash, inkling_uair, inkling_uspecial_landing, inkling_utilt     
     );
 }
